9d4536391599d971da59f40c0c3d3aaeea6704fa
geoneubie <geoneubie@gmail.com>
2015-05-08 11:12:17 -0600

    'Simple property add.'

--- csb/m77tToGeoJSON.groovy
+++ csb/m77tToGeoJSON.groovy
@@ -5,37 +5,41 @@ def lat
 def lon
 def z
 def tokens = []
-def rows = []
+def pts = []
+def ids = []
 
-new File('resources/95003.m77t').eachLine { line ->
+new File( 'resources/95003.m77t' ).eachLine { line ->
   //println line
-  tokens = line.tokenize('\t')
+  tokens = line.tokenize( '\t' )
   println tokens
   id = tokens[0]
   lat = tokens[4]
   lon = tokens[5]
   z = tokens[8]
-  rows << [id, lat, lon, z]
+  ids << id
+  pts << [lat, lon, z]
 }
 
-def jsonFile = new File('resources/95003.json')
-def ptMap = [:]
-rows.each { row ->
-
+def jsonFile = new File( 'resources/95003.json' )
+def sb = new StringBuffer( )
+def i = 0
+pts.each { pt ->
     // Create the GeoJSON feature
-    def jb = new JsonBuilder()
+    def jb = new JsonBuilder( )
     Map feature = jb {
         type 'Feature'
         geometry {
             type 'Point'
-            coordinates( [row] )
+            coordinates( [pt] )
         }
         properties {
-            id
+          "id:" ids[i]
         }
     }
 
-    jb.toPrettyString()
-    println jb
-
+    
+    sb <<= "${jb.toPrettyString( )}\n"
+    i++
 }
+
+println sb
